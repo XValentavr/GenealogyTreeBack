70b92838d1fe25673866b4f595e38e0f23a9bedd
xvalentavr <iwilly17@gmail.com>
2022-10-26 19:44:37 +0300

    added tech support

--- backend/support/admin.py
+++ backend/support/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Support
+
+
 # Register your models here.
+@admin.register(Support)
+class TechSupport(admin.ModelAdmin):
+    fields = ('name', 'date', 'email', 'phone')
+    search_fields = ('name', 'date', 'email', 'phone')
