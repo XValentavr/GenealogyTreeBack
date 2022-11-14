86fe3b42503e93d78edbe664c8cdff2ea25c7dee
xvalentavr <iwilly17@gmail.com>
2022-11-14 22:40:29 +0300

    added hooks, useContent and upgrade feedback button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import './App.css';
 import MainPage from "./components/MainPage/MainPage";
+import FeedbackProvider from "./components/MainPage/store/feedback/FeedbackProvider";
 
 function App() {
     return (
+        <FeedbackProvider>
             <MainPage/>
+        </FeedbackProvider>
     );
 }
 
