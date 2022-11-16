b41fa01dc71f0a46b4da2d043d1beba7a453be84
Valentyn Volynskyi <51284814+XValentavr@users.noreply.github.com>
2022-11-16 17:32:06 +0300

    Delete frontend directory

--- frontend/src/Components/MainPage/Card/FormWrapper.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-const FormWrapper = props => {
-    return (
-            <div className={props.inner}>
-                {props.children}
-            </div>
-    );
-}
-export default FormWrapper
--- frontend/src/Components/MainPage/Card/SectionCard.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import classes from './SectionCard.module.css'
-
-const SectionCard = props => {
-    return (
-            <section id={props.id} className={classes[props.cssClass]}>{props.children}</section>
-    );
-}
-export default SectionCard
